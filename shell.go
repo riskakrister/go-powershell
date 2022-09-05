a03c36bf10c4f84b8e9e40fd89648fa52e2e0380
Krister Riska <94383739+riskakrister@users.noreply.github.com>
2022-09-05 16:23:40 +0300

    Update shell.go

--- shell.go
+++ shell.go
@@ -8,8 +8,8 @@ import (
 	"strings"
 	"sync"
 
-	"github.com/bhendo/go-powershell/backend"
-	"github.com/bhendo/go-powershell/utils"
+	"github.com/riskakrister/go-powershell/backend"
+	"github.com/riskakrister/go-powershell/utils"
 	"github.com/juju/errors"
 )
 
